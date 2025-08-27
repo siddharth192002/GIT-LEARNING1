52e9517b7f359efd555dd568b2177654f0820926
Siddharth Tripathi <siddharthtri2002@gmail.com>
2025-08-27 15:41:43 +0530

    payement update after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login ="login page added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer ="footer added in our website"
 console.log(footer);
+=======
+const payement = "integerated payement"
+console.log(payement);
+>>>>>>> 877a00a (payement added)
