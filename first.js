590fd3038f77ebab4bd1c0a6bdf0418a5463c4e5
Siddharth Tripathi <siddharthtri2002@gmail.com>
2025-08-27 16:21:43 +0530

    latest change

--- first.js
+++ first.js
@@ -5,17 +5,16 @@ console.log(button);
 const login ="login page added";
 console.log(login);
 
-<<<<<<< HEAD
+
 const footer ="footer added in our website"
 console.log(footer);
-=======
+
 const payement = "integerated payement"
 console.log(payement);
-<<<<<<< HEAD
->>>>>>> 877a00a (payement added)
-=======
+
 
 
 const upi ="integeated the upi";
 console.log(upi);
->>>>>>> c619611 (upi added)
+
+console.log("LATEST UPDATE");
